d880a24049674208fdef61be92d676c107b028c1
maddogxx <maddogxx@gmail.com>
2017-10-19 18:19:59 -0300

    Update OrganizadorArquivosXML.java

--- OrganizadorArquivosXML.java
+++ OrganizadorArquivosXML.java
@@ -1,7 +1,12 @@
 package lab.quadronegro.app;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -39,6 +44,10 @@ public class OrganizadorArquivosXML {
 		Document doc = builder.parse(arquivo.getPath());
 		NodeList elementos = doc.getElementsByTagName("det");
 		
+		Node noDataEmissao = doc.getElementsByTagName("dEmi").item(0);
+		String dataEmissao = noDataEmissao.getFirstChild().getNodeValue();
+		dataEmissao = dataEmissao.substring(0, 4);
+		
 		for (int i = 0; i < elementos.getLength(); i++) {
 			Node elemento = elementos.item(i);
 			
@@ -47,24 +56,36 @@ public class OrganizadorArquivosXML {
 			
 			Node nomeProduto = elementosProduto.item(2).getFirstChild();
 			
+			String nomeProdutoConvertido = converterNomeProduto(nomeProduto.getNodeValue());
 			
-			
-			System.out.println(converterNomeProduto(nomeProduto.getNodeValue()));
+			new File(DIRETORIO + nomeProdutoConvertido + File.pathSeparator + dataEmissao).mkdirs(); 
 		}
 		
 		//FileUtils.copyFileToDirectory(srcFile, destDir);
 	}
 	
 	private static String converterNomeProduto(String nomeProduto) {
-		String nomeAlterado = nomeProduto;
+		String nomeProdutoAlterado = nomeProduto;
 		
-		nomeAlterado += nomeAlterado.replaceAll(" ", "_").replaceAll("\"", "");
+		nomeProdutoAlterado = nomeProdutoAlterado.replaceAll(" ", "_").replaceAll("\"", "").replaceAll("\\.", "");
 		
-		return nomeProduto;
+		return nomeProdutoAlterado;
 	}
 	
-	private static void gravarArquivo() {}
-	
-
-
+	private static void gravarArquivo(File origem, File destino) throws IOException {
+	    InputStream is = null;
+	    OutputStream os = null;
+	    try {
+	        is = new FileInputStream(origem);
+	        os = new FileOutputStream(destino);
+	        byte[] buffer = new byte[1024];
+	        int length;
+	        while ((length = is.read(buffer)) > 0) {
+	            os.write(buffer, 0, length);
+	        }
+	    } finally {
+	        is.close();
+	        os.close();
+	    }
+	}
 }
